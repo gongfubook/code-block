d40746db40c1214682c0e87cd746c8a708b6ff00
yingchao <13693288023@163.com>
2023-01-07 23:54:31 +0800

    规范代码块接口

--- block-base.cpp
+++ block-base.cpp
@@ -7,77 +7,139 @@
 #include <QDebug>
 
 
-block::block(QWidget *parent=nullptr): QLabel(parent){
-    resize(block_width+2, block_height+2);
-    block_pixmap = QPixmap(block_width, block_height);
-    block_pixmap.fill(Qt::white);
-    QPainter painter(&block_pixmap);
-    // if (!isHovered)
-    // {
-    //     const QColor color(50, 50, 63);
-    //     painter.setPen(color);
-    // }
-    // else
-    // {
-    //     const QColor color(86, 85, 96);
-    //     painter.setPen(color);
-    // }
+QPainterPath createPath(
+    int x,
+    int y,
+    int width,
+    int height,
+    enum Connector left,
+    enum Connector right,
+    enum Connector up,
+    enum Connector down
+){
+    QPainterPath path(QPointF(x, y));
+    if (up == Connector::none) {
+        path.lineTo(x + width, y);
+    } else if (up == Connector::female) {
+        path.lineTo(x + 30, y);
+        path.lineTo(x + 35, y + 5);
+        path.lineTo(x + 40, y);
+        path.lineTo(x + width, y);
+    } else {
+        path.lineTo(x + 30, y);
+        path.lineTo(x + 35, y - 5);
+        path.lineTo(x + 40, y);
+        path.lineTo(x + width, y);
+    }
 
-    const QColor color(86, 85, 96);
-    QPen pen;
-    pen.setColor(mColorBorder);
-    pen.setWidth(1);
-    painter.setPen(pen);
-    block_text = "基础块";
-    block_path = generate_path();
-    painter.drawPath(block_path);
+    if (right == Connector::none) {
+        path.lineTo(x + width, y + height);
+    } else if (right == Connector::female) {
+        path.lineTo(x + width, y + 10);
+        path.lineTo(x + width - 5, y + 5);
+        path.lineTo(x + width - 5, y + 25);
+        path.lineTo(x + width, y + 20);
+        path.lineTo(x + width, y + height);
+    } else {
+        path.lineTo(x + width, y + 10);
+        path.lineTo(x + width + 5, y + 5);
+        path.lineTo(x + width + 5, y + 25);
+        path.lineTo(x + width, y + 20);
+        path.lineTo(x + width, y + height);
+    }
 
+    if (down == Connector::none) {
+        path.lineTo(x, y + height);
+    } else if (down == Connector::female) {
+        path.lineTo(x + 40, y + height);
+        path.lineTo(x + 35, y - 5 + height);
+        path.lineTo(x + 30, y + height);
+        path.lineTo(x, y + height);
+    } else {
+        path.lineTo(x + 40, y + height);
+        path.lineTo(x + 35, y + 5 + height);
+        path.lineTo(x + 30, y + height);
+        path.lineTo(x, y + height);
+    }
 
-    QFont font;
-    font.setFamily("Microsoft YaHei");
-    // 大小
-    font.setPointSize(font_size);
-
-    // 设置字符间距
-    font.setLetterSpacing(QFont::AbsoluteSpacing, 5);
-    // 使用字体
-    painter.setFont(font);
-
-    QFontMetrics fm = painter.fontMetrics();
-    int width_text = fm.horizontalAdvance(block_text);
+    if (left == Connector::none) {
+        path.lineTo(x, y);
+    } else if (left == Connector::female) {
+        path.lineTo(x, y + 20);
+        path.lineTo(x + 5, y + 25);
+        path.lineTo(x + 5, y + 5);
+        path.lineTo(x, y + 10);
+        path.lineTo(x, y);
+    } else {
+        path.lineTo(x, y + 20);
+        path.lineTo(x - 5, y + 25);
+        path.lineTo(x - 5, y + 5);
+        path.lineTo(x, y + 10);
+        path.lineTo(x, y);
+    }
     
-    painter.drawText((this->width() - width_text) / 2, (this->height() - font_size) + (this->height() - font_size) / 3, block_text);
+    return path;
 
-    painter.end();
-    // this->setPixmap(block_pixmap);
-    move(block_x, block_y); 
-    setAcceptDrops(true);  
-    //setStyleSheet("background-color:red;");
 }
 
-QPoint block::raw_point(){
+
+block_base::block_base(QWidget *parent=nullptr): QLabel(parent){
+    move(block_x, block_y);
+    resize(block_width, block_height); 
+    setAcceptDrops(true);  
+    createPixmap();
+    setStyleSheet("background:transparent");
+    setAttribute(Qt::WA_TranslucentBackground);
+    // setStyleSheet("background-color:red;");
+}
+
+void block_base::createPixmap(){
+    block_pixmap = QPixmap(block_width, block_height);
+    block_pixmap.fill(Qt::transparent);
+    QPainter painter(&block_pixmap);
+    painter.setPen(Qt::white);
+    painter.setBrush(Qt::blue);
+    block_text = "基础块";
+    block_path = createPath(5, 5, block_width - 20, block_height - 10, block_left, block_right, block_up, block_down);
+    painter.drawPath(block_path);
+    QFont font;
+    font.setFamily("Microsoft YaHei");
+    font.setPointSize(font_size);
+    font.setLetterSpacing(QFont::AbsoluteSpacing, 5);
+
+    painter.setFont(font);
+    QFontMetrics fm = painter.fontMetrics();
+    int width_text = fm.horizontalAdvance(block_text);
+    painter.drawText((this->width() - width_text) / 2, (this->height() - font_size) + (this->height() - font_size) / 3, block_text);
+    painter.end();
+}
+
+void block_base::setPixmap(const QPixmap& pixmap){
+    block_pixmap = pixmap;
+}
+
+QPixmap block_base::getPixmap(){
+    return block_pixmap;
+}
+
+void block_base::setPoint(const QPoint& point){
+    block_x = point.x();
+    block_y = point.y();
+}
+
+QPoint block_base::getPoint(){
     return QPoint(block_x, block_y);
 }
 
-
-QPainterPath block::generate_path(){
-    int start_x = 0;
-    int start_y = 0;
-    QPainterPath path(QPointF(start_x, start_y));
-    path.lineTo(start_x + block_width, start_y);
-    path.lineTo(start_x + block_width, start_y + block_height);
-    path.lineTo(start_x, start_y + block_height);
-    path.lineTo(start_x, start_y);
-    return path;
-    
+QString block_base::toCode(){
+    return code_text;
 }
 
-
-void block::paintEvent(QPaintEvent *event)
+void block_base::paintEvent(QPaintEvent *event)
 {
     Q_UNUSED(event);
     QPainter painter(this);
-    painter.drawPixmap(QPoint(0,0), block_pixmap);
+    painter.drawPixmap(QRect(0, 0, block_pixmap.width(), block_pixmap.height()), block_pixmap);
 }
 
 // void block::mousePressEvent(QMouseEvent *event){
--- block-base.h
+++ block-base.h
@@ -8,30 +8,64 @@
 #include <Qcolor>
 #include <QPainterPath>
 #include <QMouseEvent>
+#include <QPixmap>
 #include <QLabel>
 
-class block : public QLabel{
+/**
+ * 代码块的接口类
+*/
+
+class block{
+    virtual void createPixmap() = 0;
+    virtual void setPixmap(const QPixmap& pixmap) = 0;
+    virtual QPixmap getPixmap() = 0;
+    virtual void setPoint(const QPoint& point) = 0;
+    virtual QPoint getPoint() = 0;
+    virtual QString toCode() = 0;
+};
+
+enum Connector{
+    none = 0,
+    male = 1,
+    female = 2
+};
+
+QPainterPath createPath(
+    int x,
+    int y,
+    int width,
+    int height,
+    Connector left,
+    Connector right,
+    Connector up,
+    Connector down
+);
+
+class block_base : public QLabel, block{
 public:
     uint32_t block_x{20}, block_y{20}, block_width{220}, block_height{50};
-    bool block_left{false}, block_right{false}, block_up{false}, block_down{false};
-    block(QWidget *parent);
-    QPainterPath generate_path();
-    QPoint raw_point();    
-    QPainterPath block_path;
-    QString block_text;
-    QPixmap block_pixmap;
+    Connector block_left{male}, block_right{female}, block_up{female}, block_down{male};
+    block_base(QWidget *parent);
+    virtual void createPixmap();
+    virtual void setPixmap(const QPixmap& pixmap);
+    virtual QPixmap getPixmap();
+    virtual void setPoint(const QPoint& point);
+    virtual QPoint getPoint();
+    virtual QString toCode();
+
 protected:
     void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
     // void mousePressEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
     // void mouseReleaseEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
     // void mouseMoveEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
 private:
-    //背景颜色
-    QColor mColorBack = Qt::white;
-    //边框颜色
-    QColor mColorBorder = Qt::black;
+    QColor color_back = Qt::white;
     int font_size{24};
     QPoint startPos;
+    QPainterPath block_path;
+    QString block_text;
+    QPixmap block_pixmap;
+    QString code_text;
 
 };
 
