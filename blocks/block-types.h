f1f76a77fd57c22e48f0127d1c2908880f566d16
yingchao <13693288023@163.com>
2023-02-18 23:28:19 +0800

    使用 BlockConnector 结构管理一个基本块的接口

--- blocks/block-types.h
+++ blocks/block-types.h
@@ -12,10 +12,20 @@ enum Connector{
     female = 2
 };
 
+/**
+ * 一个块四个方位的连接口
+*/
+struct BlockConnector{
+    Connector up, right, down, left;
+};
+
+/**
+ * 块类型
+*/
 enum BlockType{
-    normal = 0,
-    pluggable = 1,
-    with_inside = 2
+    normal = 0,               // 普通方块
+    pluggable = 1,            // 可连接块
+    with_inside = 2           // 内部包含其它块
 };
 
 
